6603eff7fc0cec92c13fb68ac62306bb4ddc751b
CHardy <christianrh2000@hotmail.com>
2022-07-04 10:15:34 -0300

    2022-07-04 10:15 hs

--- app-peliculas/src/App.jsx
+++ app-peliculas/src/App.jsx
@@ -1,26 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
 
 import './App.css';
-
 import ListaProductosPagina from './paginas/ListaProductosPagina';
 import SearchMoviePage from './pages/SearchMoviePage';
 import MovieDetailPage from "./pages/MovieDetailPage";
 
+// Create a client
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div> Home ... </div>} /> 
-        <Route path="productos" element={<ListaProductosPagina />} />
-        <Route path="peliculas" element={<SearchMoviePage />} />
-        <Route path="peliculas/:idMovie" element={<MovieDetailPage />} />
-        <Route path="*" element={<div> Pagina No Encontrada. Error 404.</div> }/>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div> Home ... </div>} /> 
+          <Route path="productos" element={<ListaProductosPagina />} />
+          <Route path="peliculas" element={<SearchMoviePage />} />
+          <Route path="peliculas/:idMovie" element={<MovieDetailPage />} />
+          <Route path="*" element={<div> Pagina No Encontrada. Error 404.</div> }/>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- app-peliculas/src/components/movies/ListMovie/index.jsx
+++ app-peliculas/src/components/movies/ListMovie/index.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react"
+import { useQuery } from "react-query"
+import { Link } from "react-router-dom"
 import { getMovies } from "../../../services/getMovie"
 import Loading from "../../general/Loading/Loading"
 import MyPagination from "../../general/MyPagination"
@@ -8,40 +10,30 @@ import "./ListMovie.css"
 const Movies = ({movies}) => {
     return(
         <section className="list-movie">
-            {movies && movies.map((val, index) => <MovieItem key={index} {...val} />)}
+            {movies && movies.map((val, index) => 
+                <Link key={index} to={val.imdbID} target="_blank" >
+                    <MovieItem {...val} />
+                </Link>
+            )}
         </section>
     )
 }
 
 const ListMovies = ({busqueda}) => {
-    //console.log(props.busqueda);
-    //const movies = [1,2,3,4,5,6,7,8,9,10].map(val => <MovieItem />)  
 
-    const [movies, setMovies] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+    // Queries (react-query)
+    //const { isLoading, isError, data, } = useQuery('movies', async () => getMovies(busqueda,pagina))
+    const [data, setData] = useState();
     const [pagina, setPagina] = useState(1);
-    const [cantidadPaginas, setCantidadPaginas] = useState();
-;
-    const getMoviesFromService = async (busqueda,pagina) => {
-        setLoading(true);
+    const [isLoading, setIsLoading] = useState(false);
 
-        const respuesta = await getMovies(busqueda,pagina);
-        if (respuesta.Error){
-            setError(respuesta.Error);
-        } else {
-            const totalPaginas = Math.ceil(parseInt(respuesta.totalResults)/10);
-            setMovies(respuesta.Search);
-            setCantidadPaginas(totalPaginas);
-        }
-
-        setLoading(false);
-
+    const getMoviesFromService = async () => {
+        setIsLoading(true);
+        const respuesta = await getMovies(busqueda, pagina);
+        setData(respuesta);
+        setIsLoading(false);
     }
-
-    const onChangePaginacion = (_evento,pag) => {
-        setPagina(pag);
-    }
+    
 
     useEffect(() => {
         if(busqueda) {
@@ -49,29 +41,33 @@ const ListMovies = ({busqueda}) => {
         }
     },[busqueda,pagina])
 
+    const onChangePaginacion = (_evento,pag) => {
+        setPagina(pag);
+    }
+
     
 // Si se esta realizando una busqueda y aun no recibo la lista de peliculas
 // muestro el Loading
-    if(loading) {
+    if(isLoading) {
         return <Loading />
     }
 
     
-    if(error) return <div>{error}</div>;
+    //if(isError) return <div>Algo ha salido mal...</div>;
 
 // Si no hay peliculas ni busqueda activa no muestro nada
 // sirve para la primera ves que entro a la pagina
-    if(!movies){
+    if (!data || !data.Search) {
         return null;
     }
 
-
-
+    const cantidadPaginas = Math.ceil(parseInt(data.totalResults)/10);
+    
 // cuando termina la busqueda y recibo la lista de peliculas
 // muestro mis componentes
     return (
         <>
-        <Movies movies={movies} />
+        <Movies movies={data.Search} />
         < MyPagination count={cantidadPaginas} page={pagina} onChange={onChangePaginacion} />
         </>
     )
